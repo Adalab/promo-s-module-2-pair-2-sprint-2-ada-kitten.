49cab351f3ad3893f71bddf3d6d7bfde81c831e5
Virlucero <virlucero@gmail.com>
2023-02-23 10:27:58 +0000

    add DOM

--- 01-solucion-sprint-1/js/main.js
+++ 01-solucion-sprint-1/js/main.js
@@ -41,27 +41,48 @@ const kittenData_3 = {
 
 //Funciones
 function renderKitten(kittenData) {
-    const kitten = `<li class="card">
-    <article>
-      <img
-        class="card_img"
-        src=${kittenData.image}
-        alt="gatito"
-      />
-      <h3 class="card_title">${kittenData.name}</h3>
-      <h3 class="card_race">${kittenData.race}</h3>
-      <p class="card_description">
-      ${kittenData.desc}
-      </p>
-    </article>
-    </li>`;
-    return kitten;
+    
+  const liElement = document.createElement('li');
+  liElement.classList.add('card');
+
+  const articleElement = document.createElement('article');
+  liElement.appendChild(articleElement);
+
+  const imgElement = document.createElement('img');
+  imgElement.classList.add('card_img');
+  imgElement.src = kittenData.image;
+  articleElement.appendChild(imgElement);
+
+  const h3Title = document.createElement('h3');
+  h3Title.classList.add('card_title');
+  articleElement.appendChild(h3Title);
+
+  const textForTitle = document.createTextNode(kittenData.name);
+  h3Title.appendChild(textForTitle);
+
+    const pDesc = document.createElement('desc');
+  pDesc.classList.add('card_description');
+  articleElement.appendChild(pDesc);
+
+  const textForDesc = document.createTextNode(kittenData.desc);
+  pDesc.appendChild(textForDesc);
+
+  return liElement;
+
 }
+
 
 function renderKittenList(kittenDataList) {
     listElement.innerHTML = "";
+
     for (const kittenItem of kittenDataList) {
-        listElement.innerHTML += renderKitten(kittenItem);
+
+      const liElement = renderKitten(kittenItem);
+
+      //liElement.addEventListener('click', handle)
+
+      listElement.appendChild(liElement);
+        
     }
 }
 
